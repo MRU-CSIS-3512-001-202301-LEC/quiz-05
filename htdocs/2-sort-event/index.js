fafe53f9efad9da645d7e753f813f14999c37298
Jordan Pratt <jpratt@mtroyal.ca>
2023-03-30 09:59:56 -0600

    Tweak suggested soln.

--- htdocs/2-sort-event/index.js
+++ htdocs/2-sort-event/index.js
@@ -1,29 +1,29 @@
+// http://127.0.0.1:8080/2-sort-event/
+
 const LAPS = [8, 12, 6.5, 111, 16, 9, 8];
 
 const BTN = document.querySelector("button");
+const BODY = document.querySelector("body");
 
-function ol(liItems) {
+function orderedListFrom(nums) {
   const ol = document.createElement("ol");
-  ol.append(...liItems);
+  const liElems = nums.map((num) => asListElem(num));
+  ol.append(...liElems);
   return ol;
 }
 
-function asLi(num) {
+function asListElem(num) {
   const li = document.createElement("li");
   li.textContent = num;
   return li;
 }
 
-let lapList = LAPS.map((lap) => asLi(lap));
-
-document.body.append(ol(lapList));
-
 function sortList() {
-  const OL = document.querySelector("ol");
+  document.querySelector("ol").remove(); // kill old one
   LAPS.sort((a, b) => a - b);
-  let lapList = LAPS.map((lap) => asLi(lap));
-  OL.replaceChildren(); // gotcha!
-  OL.append(...lapList);
+  BODY.append(orderedListFrom(LAPS));
 }
+
+BODY.append(orderedListFrom(LAPS));
 
 BTN.addEventListener("click", sortList);
